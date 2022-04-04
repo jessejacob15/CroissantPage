1c9db3571469b43aa321544a9fad7fb4fd842334
Jesse Jacob <jjacob@colgate.edu>
2022-04-04 15:57:13 -0400

    fixed typo in text

--- src/components/Textbox/Textbox.js
+++ src/components/Textbox/Textbox.js
@@ -8,9 +8,9 @@ const Textbox = () => {
         make impact through providing crypto donations to those in need through
         the NYC FOOD BANK, & documenting the impact of ever so important social
         workers. Croissants are flakey, but we are not. We aim to establish
-        partnerships with social organizations & bakery’s throughout the world.
+        partnerships with social organizations & bakeries throughout the world.
         Pastries are sweet & comforting, with the power of community and
-        decentralized finance there is no reason everyone shouldn’t be able to
+        decentralized finance there is no reason everyone should not be able to
         have a bite. -CROissant Team
       </p>
     </div>
