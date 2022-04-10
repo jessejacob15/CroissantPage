0c540f05e47cdc66567e94e2321e1871f28c5c58
Jesse Jacob <jjacob@colgate.edu>
2022-04-09 21:20:24 -0400

    changing text

--- src/components/Textbox/Textbox.js
+++ src/components/Textbox/Textbox.js
@@ -4,10 +4,10 @@ const Textbox = () => {
   return (
     <div className="square">
       <p className="textMain">
-        CROissant is the first food token on the CRO network. We are a mix of
-        creativity and philanthropy. In addition, we strive to educate the
-        public about crypto, help small business, and provide philanthropy while
-        creating content to share it with the world.
+        CROissant is the first food token on the CRONOS network. We are a mix of
+        creativity and philanthropy. We strive to educate the public about
+        crypto, help small business, and provide philanthropy while creating
+        content to share it with the world.
       </p>
     </div>
   );
