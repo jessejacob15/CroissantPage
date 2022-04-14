d73e4e0470ec20a218f24fdd37689edff481ead3
Jesse Jacob <jjacob@colgate.edu>
2022-04-13 22:42:40 -0400

    new web3 attempt

--- src/web3.js
+++ src/web3.js
@@ -1,7 +1,13 @@
 import Web3 from "web3";
 
-window.ethereum.request({ method: "eth_requestAccounts" });
+// window.ethereum.request({ method: "eth_requestAccounts" });
 
-const web3 = new Web3(window.ethereum);
+// const web3 = new Web3(window.ethereum);
 
+// export default web3;
+
+const web3 = new Web3(
+  Web3.givenProvider ||
+    "https://rinkeby.infura.io/v3/15c1d32581894b88a92d8d9e519e476c"
+);
 export default web3;
