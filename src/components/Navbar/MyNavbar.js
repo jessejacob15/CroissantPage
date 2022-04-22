0b4e687afee817fb87201ed02b1dbc4b5c9226ac
Jesse Jacob <jjacob@colgate.edu>
2022-04-22 15:30:12 -0400

    open links in new tab

--- src/components/Navbar/MyNavbar.js
+++ src/components/Navbar/MyNavbar.js
@@ -10,23 +10,30 @@ const Navigation = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="m-auto">
-              <Nav.Link href="https://t.me/croissantdao">
+              <Nav.Link href="https://t.me/croissantdao" target="_blank">
                 <h1>Telegram</h1>
               </Nav.Link>
-              <Nav.Link href="https://vm.tiktok.com/ZTdxousAF/">
+              <Nav.Link href="https://vm.tiktok.com/ZTdxousAF/" target="_blank">
                 <h1>TikTok</h1>
               </Nav.Link>
-              <Nav.Link href="https://twitter.com/CroissantDAO">
+              <Nav.Link href="https://twitter.com/CroissantDAO" target="_blank">
                 <h1>Twitter</h1>
               </Nav.Link>
-              <Nav.Link href="https://dexscreener.com/cronos/0xd4bdc69abbb9995f67f0306d19bd3fc1a0ccef5e">
+              <Nav.Link
+                href="https://dexscreener.com/cronos/0xd4bdc69abbb9995f67f0306d19bd3fc1a0ccef5e"
+                target="_blank"
+              >
                 <h1>Chart</h1>
               </Nav.Link>
-              <Nav.Link href="https://croissantdao.com/CroissantDao.pdf">
+              <Nav.Link
+                href="https://croissantdao.com/CroissantDao.pdf"
+                target="_blank"
+              >
                 <h1>Whitepaper</h1>
               </Nav.Link>
               <Nav.Link
                 href="https://mm.finance/swap?outputCurrency=0xae443ed39257d1346b0c71b5bc258afd5bcd7ce8"
+                target="_blank"
                 className="buyBox"
               >
                 <div className="buyButton">Buy</div>
